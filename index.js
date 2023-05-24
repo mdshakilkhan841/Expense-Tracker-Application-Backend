8b3027aca1c973c4cda220d442cd6b210f435dca
mdshakilkhan841 <mdshakilkhan841@gmail.com>
2023-05-25 00:14:10 +0600

    expense model created

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from "express"; //valid only for node.js latest version 
 import dotenv from 'dotenv';
+import cors from 'cors';
 import Connection from "./database/Connection.js";
+import route from "./routes/route.js";
 
 const app = express();
 dotenv.config()
+
+app.use(cors()); // to resolve cors issue
+app.use('/', route)
 
 const PORT = process.env.PORT || 8000;
 const username = process.env.DB_USERNAME;
