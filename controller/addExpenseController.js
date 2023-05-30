1becac7796808561f87da7ead6db955f93602d88
mdshakilkhan841 <mdshakilkhan841@gmail.com>
2023-05-31 00:21:19 +0600

    Router and controller updated

--- controller/addExpenseController.js
+++ controller/addExpenseController.js
@@ -19,7 +19,7 @@ export const addExpense = async (request, response) => {
         response.status(409).json({ message: error.message });
     }
 
-}
+};
 
 // get expense data from DB
 export const getExpenses = async (request, response) => {
@@ -29,17 +29,17 @@ export const getExpenses = async (request, response) => {
     } catch (error) {
         response.status(404).json({ message: error.message });
     }
-}
+};
 
-// get date wise expense data from DB
-export const getDateExpenses = async (request, response) => {
+// get expense data from DB
+export const getExpensesList = async (request, response) => {
     try {
         const expenseData = await expenseRecord.aggregate([
-            { $sort: { dateTime: -1 } },
+            { $sort: { expenseId: -1 } },
             {
                 $project: {
                     _id: 0,
-                    _id: 0,
+                    expenseId: 1,
                     itemName: 1,
                     amount: 1,
                     category: 1,
@@ -58,6 +58,80 @@ export const getDateExpenses = async (request, response) => {
         response.status(404).json({ message: error.message });
     }
 };
+
+// get date wise expense data from DB
+export const getDateExpenses = async (request, response) => {
+    try {
+        const expenseData = await expenseRecord.aggregate([
+            { $sort: { dateTime: -1 } },
+            {
+                $project: {
+                    _id: 0,
+                    expenseId: 1,
+                    itemName: 1,
+                    amount: 1,
+                    category: 1,
+                    dateTime: 1,
+                    date: {
+                        $dateToString: {
+                            format: "%d/%m/%Y",
+                            date: { $toDate: "$dateTime" }  // Convert "dateTime" field to date
+                        }
+                    }
+                }
+            }
+        ]);
+        response.status(200).json(expenseData);
+    } catch (error) {
+        response.status(404).json({ message: error.message });
+    }
+};
+
+// get single expense data from DB
+export const getExpense = async (request, response) => {
+    // console.log(request.params)
+    try {
+        const expenseData = await expenseRecord.find({expenseId: request.params.expenseId});
+        response.status(200).json(expenseData);
+    } catch (error) {
+        response.status(404).json({ message: error.message });
+    }
+};
+
+
+//update edit expense form data 
+export const editExpense = async (request, response) => {
+    const expense = request.body;
+
+    const { itemName, amount, category, dateTime } = expense;
+    if (!itemName || !amount || !category || !dateTime) {
+        response.status(404).send("Please fill the data !");
+    }
+
+    console.log(expense);
+
+    const editExpense = new expenseRecord(expense);
+
+    try {
+        await expenseRecord.updateOne({expenseId: request.params.expenseId}, editExpense);
+        response.status(201).json(editExpense);
+    } catch (error) {
+        response.status(409).json({ message: error.message });
+    }
+
+};
+
+
+// Delete single expense data from DB
+export const deleteExpense = async (request, response) => {
+    try {
+        await expenseRecord.deleteOne({expenseId: request.params.expenseId});
+        response.status(200).json({ message: 'Expense Deleted Successfully'});
+    } catch (error) {
+        response.status(409).json({ message: error.message });
+    }
+};
+
 
 
 
--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,22 @@
 import  express from "express";
-import { addExpense, getExpenses, getDateExpenses } from "../controller/addExpenseController.js";
+import { addExpense, getExpense, getExpensesList, getExpenses, getDateExpenses, editExpense, deleteExpense } from "../controller/addExpenseController.js";
 
 const router = express.Router();
 
 router.post('/', addExpense)
-router.get('/expense-list', getExpenses)
+
+router.get('/expense-list/:expenseId', getExpense)
+router.get('/expense-report/:expenseId', getExpense)
+
+router.put('/expense-list/:expenseId', editExpense)
+
+router.get('/expense-list', getExpensesList)
 router.get('/expense-report', getDateExpenses)
 router.get('/category-report', getExpenses)
 router.get('/forecasted-expenses', getDateExpenses)
+
+router.delete('/expense-list/:expenseId', deleteExpense)
+router.delete('/expense-report/:expenseId', deleteExpense)
 
 
 export default router;
